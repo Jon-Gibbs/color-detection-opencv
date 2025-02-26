03bab9eefd838145252e795b1ea1abeee5d98400
LargeFlipFlop <jon@gibbs.tk>
2025-02-26 09:51:59 -0800

    Followed tutorial

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 from PIL import Image
-
 from util import get_limits
+#this program accesses the machines webcam, takes the video captured by it, and displays it in a window onscreen
 
+yellow = [0, 0, 0] # yellow in brg
+#initialize a connection to the webcam and store that connection in the "cap" object
+cap=cv2.VideoCapture(0)
 
-yellow = [0, 255, 255]  # yellow in BGR colorspace
-cap = cv2.VideoCapture(2)
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() #captures a frame from the webcam and stores the image in the frame variable, ret is a boolean that returns true if the the image was captured properly, and false if not
 
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -15,21 +16,24 @@ while True:
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
-    mask_ = Image.fromarray(mask)
+    mask_ = Image.fromarray(mask )
 
-    bbox = mask_.getbbox()
+    boundingBox = mask_.getbbox()
 
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
+    if boundingBox is not None:
+        x1,y1,x2,y2 = boundingBox
 
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 5)
 
-    cv2.imshow('frame', frame)
+    print(boundingBox)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('the guy' , frame) #takes the image stored in "frame" and posts it in the window using the cv2.imshow function
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): # breaks the loop is the user presses q
         break
 
-cap.release()
+cap.release() #releases the camera (a bit like calling destroy)
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() #cleans up any hanging windows the program made
+
 
